dc15163aea9f28e2223d6f5bf33f7185292a6908
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-06-10 15:10:23 -0600

    Add event listeners

---
+++ main.js
@@ -0,0 +1,9 @@
+var titleInput = document.querySelector("#left__input--title")
+var urlInput = document.querySelector("#left__input--url")
+var enterBtn = document.querySelector(".left__form--btn")
+
+/**************Event Lsiteners*******************/
+
+titleInput.addEventListener("click")
+urlInput.addEventListener("click")
+enterBtn.addEventListener("click")
