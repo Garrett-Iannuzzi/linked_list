63adde29017895edd6cb86fd9da8eef94ce04c75
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-06-10 16:48:37 -0600

    Add card population to js

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 var titleInput = document.querySelector("#left__input--title")
 var urlInput = document.querySelector("#left__input--url")
 var enterBtn = document.querySelector(".left__form--btn")
+var rightContainer = document.querySelector(".right__container")
 
 /**************Event Lsiteners*******************/
+enterBtn.addEventListener("click", displayBookmark)
 
-titleInput.addEventListener("click")
-urlInput.addEventListener("click")
-enterBtn.addEventListener("click")
+/*****************Function***********************/
+
+function displayBookmark(e) {
+	e.preventDefault();
+  rightContainer.insertAdjacentHTML(
+    "afterbegin",
+			`<article class="right__section--article">
+				<h2 class="right__article--title">Title</h2>
+				<link herf="Link" type="Link here">
+				<button class="right__article--read">Read</button>
+				<button class="right__article--delete">Delete</button>
+				</article>`
+    )
+}
