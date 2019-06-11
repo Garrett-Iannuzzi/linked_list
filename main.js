3391d99faf2f47ea630c8e529105e4a8c34c215b
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-06-10 20:26:33 -0600

    Add article cards

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ var titleInput = document.querySelector("#left__input--title")
 var urlInput = document.querySelector("#left__input--url")
 var enterBtn = document.querySelector(".left__form--btn")
 var rightContainer = document.querySelector(".right__container")
+var readBtn = document.querySelector("right__article-read")
 
 /**************Event Lsiteners*******************/
 enterBtn.addEventListener("click", displayBookmark)
+readBtn.addEventListener("click", addToReadList)
+// readBtn.addEventListener("click", deleteFromReadList )
 
 /*****************Function***********************/
 
@@ -13,10 +16,20 @@ function displayBookmark(e) {
   rightContainer.insertAdjacentHTML(
     "afterbegin",
 			`<article class="right__section--article">
-				<h2 class="right__article--title">Title</h2>
-				<link herf="Link" type="Link here">
-				<button class="right__article--read">Read</button>
-				<button class="right__article--delete">Delete</button>
+				<h2 class="right__article--title">${titleInput.value}</h2>
+				<h2 class="right__article--url">${urlInput.value}</h2>
+				<div class="right__article--buttons">
+					<button class="right__article--read">Read</button>
+					<button class="right__article--delete">Delete</button>
+				</div>	
 				</article>`
     )
 }
+
+function addToReadList() {
+	
+}
+
+// function deleteFromReadList() {
+
+// }
