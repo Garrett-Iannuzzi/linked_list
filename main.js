69799903bab05396738f112d015a98022e320341
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-06-13 11:48:11 -0600

    Add .read class function

--- main.js
+++ main.js
@@ -2,34 +2,44 @@ var titleInput = document.querySelector("#left__input--title")
 var urlInput = document.querySelector("#left__input--url")
 var enterBtn = document.querySelector(".left__form--btn")
 var rightContainer = document.querySelector(".right__container")
-var readBtn = document.querySelector("right__article-read")
 
 /**************Event Lsiteners*******************/
 enterBtn.addEventListener("click", displayBookmark)
-readBtn.addEventListener("click", addToReadList)
-// readBtn.addEventListener("click", deleteFromReadList )
+titleInput.addEventListener("keydown", disableBtns)
+urlInput.addEventListener("keydown", disableBtns)
 
 /*****************Function***********************/
-
 function displayBookmark(e) {
 	e.preventDefault();
   rightContainer.insertAdjacentHTML(
-    "afterbegin",
-			`<article class="right__section--article">
-				<h2 class="right__article--title">${titleInput.value}</h2>
-				<h2 class="right__article--url">${urlInput.value}</h2>
-				<div class="right__article--buttons">
-					<button class="right__article--read">Read</button>
-					<button class="right__article--delete">Delete</button>
-				</div>	
-				</article>`
+		"afterbegin",
+		`<article class="right__section--article">
+		<h2 class="right__article--title">${titleInput.value}</h2>
+		<a href="${urlInput.value}" class="right__article--url">${urlInput.value}</a>
+		<div class="right__article--buttons">
+		<button class="right__article--read">Read</button>
+		<button class="right__article--delete">Delete</button>
+		</div>	
+		</article>`
     )
-}
-
-function addToReadList() {
+		var readBtns = document.querySelectorAll(".right__article--read")
+		for (var i = 0; i < readBtns.length; i++){
+			readBtns[i].addEventListener("click", addReadClass);
+		}
+	}
 	
+	function addReadClass(e) {
+		if (e.target.classList.contains(".read")) {
+			e.target.classList.remove(".read");
+		} else {
+			e.target.classList.add(".read");
+		}
+	}
+
+
+function disableBtns() {
+	if (titleInput.value.length > 0 && urlInput.value.length > 0){
+		document.querySelector(".left__form--btn").disabled = false;
+	}else {document.querySelector(".left__form--btn").disabled = true;
+	}
 }
-
-// function deleteFromReadList() {
-
-// }
